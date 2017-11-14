6baf2130ae8a89b43837fe7df52623542f5fed83
Daisune <33661479+Daisune@users.noreply.github.com>
2017-11-14 18:03:26 -0500

    added 2 scripts
    One unlocks account, other displays all locked accounts in AD

--- ADUnlock.ps1
+++ ADUnlock.ps1
@@ -1,8 +1,8 @@
-﻿$Credentials = Get-MyCredential (join-path ($PsScriptRoot) Syncred.xml)
+﻿$Credentials = Get-Credential
 $User = Read-Host -Prompt "Input AD User"
 $lock = get-aduser $user -Properties LockedOut | select lockedout -OutVariable lock
 If ($lock -eq "False") { Write-Output "$user is not locked out" } 
 Else {Unlock-ADAccount $user -Credential $Credentials
 Write-Output "User should be unlocked now"
-get-aduser $user -Properties LockedOut | select lockedout
+get-aduser -filter * -Properties LockedOut | where lockedout -eq true | select name,lockedout
  }
---
+++ LockedChecker.ps1
@@ -0,0 +1,3 @@
+﻿get-aduser -filter * -Properties LockedOut | where lockedout -eq true | select name,lockedout | ft
+
+Read-Host 'Press Enter to continue…' | Out-Null
