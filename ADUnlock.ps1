31ca22246ce227003839bdd96769b63367174515
Daisune <33661479+Daisune@users.noreply.github.com>
2017-11-14 10:02:18 -0500

    ADUnlock + Rollout script

---
+++ ADUnlock.ps1
@@ -0,0 +1,8 @@
+﻿$Credentials = Get-MyCredential (join-path ($PsScriptRoot) Syncred.xml)
+$User = Read-Host -Prompt "Input AD User"
+$lock = get-aduser $user -Properties LockedOut | select lockedout -OutVariable lock
+If ($lock -eq "False") { Write-Output "$user is not locked out" } 
+Else {Unlock-ADAccount $user -Credential $Credentials
+Write-Output "User should be unlocked now"
+get-aduser $user -Properties LockedOut | select lockedout
+ }
